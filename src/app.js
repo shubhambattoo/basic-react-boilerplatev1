d412f10ce0e97d35a204996039f3017a0bf3609f
shubham <shubham.battoo@hotmail.com>
2018-09-08 21:02:30 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+ReactDom.render(<p>This is my boilerplate</p>, document.getElementById('container'))
